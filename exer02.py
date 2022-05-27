733de4a76d46e4ddcade486df63c19a6cc595f77
oromero <oromero@unsa.edu.pe>
2022-05-26 21:27:15 -0500

    exer01 exer02 pasaron test

---
+++ exer02.py
@@ -0,0 +1,18 @@
+def esEscalar(m):
+  d = m[0][0]
+  for i in range(len(m)):
+    for j in range(len(m)):
+      if i != j:
+        if m[i][j] != 0:
+          print(m[i][j])
+          return False
+      elif m[i][j] != d:
+        print(m[i][j])
+        return False
+  return True
+
+def esUnitaria(m):
+  return m[0][0] == 1 and esEscalar(m)
+
+x = [[1,0],[0,2]]
+print(esUnitaria(x))
